c10b3569874e2b326b1d2d2d575af2d89b413d1b
Rebecca Miles <remiles@wpi.edu>
2018-11-29 17:25:48 -0500

    made nicer schedule creation form

--- TeamWezenSchedule/html/organizerscript.js
+++ TeamWezenSchedule/html/organizerscript.js
@@ -1,4 +1,14 @@
-
+function handleSubmit(){
+  document.getElementById("orgtools").style.display = 'none';
+  createSchedule();
+  return false;  
+}
 function createSchedule(){
-  alert("Schedule Created");
+  // alert("Schedule Created");
+  let startDay = document.getElementById("setstartday").value;
+  let endDay = document.getElementById("setendday").value;
+  let timeslot = document.getElementById("settimeslots").value;
+  let dailyStartTime = document.getElementById("settimestart").value;
+  let dailyEndTime = document.getElementById("settimeend").value;
+  alert("end time: "+dailyEndTime);
 }
