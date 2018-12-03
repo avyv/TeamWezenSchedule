e8721439927a877c60a501a6c60096ed856d891d
Rebecca Miles <remiles@wpi.edu>
2018-12-03 15:55:01 -0500

    Updated Organizer Input

--- TeamWezenSchedule/html/organizerscript.js
+++ TeamWezenSchedule/html/organizerscript.js
@@ -19,30 +19,41 @@ function checkValidResponse(){
   let sid = (document.getElementById("setschedid").value != "");
   let sd = (document.getElementById("setstartday").value != "");
   let ed = (document.getElementById("setendday").value != "");
-  let ts = (document.getElementById("settimeslots").value != "");
-  let dst = (document.getElementById("settimestart").value != "");
-  let det = (document.getElementById("settimeend").value != "");
-
-  return (sd && ed && ts && dst && det && sid);
+  return (sd && ed && sid);
 }
 
-
+function updateEndTime(){
+  let start = document.getElementById("settimestart").value;
+  let end = document.getElementById("settimeend");
+  end.innerHTML = "";
+  let st = start.split(":");
+  let hrs = parseInt(st[0]) + 1;
+  let mins = parseInt(st[1]);
+  while(hrs<=24){
+    label = hrs + ":" + 0 + mins;
+    let option = document.createElement("OPTION");
+    option.innerHTML=label;
+    option.value = label;
+    end.appendChild(option);
+    hrs++;
+  }
+}
 
 function createSchedule(){
   let sid = document.getElementById("setschedid").value
   let startDay = document.getElementById("setstartday").value;
   let endDay = document.getElementById("setendday").value;
-  let timeslot = document.getElementById("settimeslots").value;
   let dailyStartTime = document.getElementById("settimestart").value;
   let dailyEndTime = document.getElementById("settimeend").value;
+  let slotDuration = document.getElementById("settimeslots").value;
   let data = {};
+  data["id"] = String(sid);
   data["startDate"] = String(startDay);
   data["endDate"] = String(endDay);
   data["startTime"] = String(dailyStartTime);
   data["endTime"] = String(dailyEndTime);
-  data["slotDuration"] = String(timeslot);
-  data["id"] = String(sid);
-  sendOrgData(data,createSchedule_url,processCreateScheduleResponse);
+  data["slotDuration"] = String(slotDuration);
+  sendOrgData(data,createSchedule_url);
 }
 function processCreateScheduleResponse(xhrResult) {
 	console.log("result: " + xhrResult);
@@ -51,7 +62,12 @@ function processCreateScheduleResponse(xhrResult) {
   alert(response);
 }
 
-function sendOrgData(data,url,processcallback){
+
+
+
+
+/*********************** SUBMIT DATA TO JAVA *********************************/
+function sendOrgData(data,url){
   let js = JSON.stringify(data);
   console.log("JS:" + js);
   let xhr = new XMLHttpRequest();
@@ -62,9 +78,18 @@ function sendOrgData(data,url,processcallback){
     console.log(xhr.request);
     if(xhr.readyState == XMLHttpRequest.DONE) {
       console.log("XHR:" + xhr.responseText);
-      processcallback(xhr.responseText);
+      processSchedule(xhr.responseText);
     } else {
-      processcallback(xhr.responseText);
+      processSchedule(xhr.responseText);
     }
   };
+}
+
+function processSchedule(xhrResult){
+	console.log("result:" + xhrResult);
+	let js = JSON.parse(xhrResult);
+  let responseSchedule = js["responseSchedule"];
+  alert("received Schedule");
+  //add if schedule couldnt be opened, display "could not find schedule"
+  // generateCalendar();
 }
