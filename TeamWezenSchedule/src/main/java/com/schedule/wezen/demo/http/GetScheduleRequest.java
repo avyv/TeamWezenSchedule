9a8bb185343fbe362c055a8d27852ba4ad2e1f09
avyannaconevargas <avyannaconevarga@wpi.edu>
2018-12-06 16:35:42 -0500

    Revert "Revert "testing""
    This reverts commit 0e214ec323b7179640c0116ea3f31bac507d7f2b.

--- TeamWezenSchedule/src/main/java/com/schedule/wezen/demo/http/GetScheduleRequest.java
+++ TeamWezenSchedule/src/main/java/com/schedule/wezen/demo/http/GetScheduleRequest.java
@@ -2,7 +2,7 @@ package com.schedule.wezen.demo.http;
 
 public class GetScheduleRequest {
 	public final String requestSchedID;
-	public final String requestWeekStart; // added week start
+	public final String requestWeekStart;
 	
 	public GetScheduleRequest (String requestSchedID, String requestStartDateOfWeek) {
 		this.requestSchedID = requestSchedID;
