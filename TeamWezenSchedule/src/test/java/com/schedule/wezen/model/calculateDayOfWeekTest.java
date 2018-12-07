27f93e7c1c9783532ebc7b7dc073cbfa69f6f3b2
avyannaconevargas <avyannaconevarga@wpi.edu>
2018-12-07 18:16:39 -0500

    Added test cases

--- TeamWezenSchedule/src/test/java/com/schedule/wezen/model/calculateDayOfWeekTest.java
+++ TeamWezenSchedule/src/test/java/com/schedule/wezen/model/calculateDayOfWeekTest.java
@@ -15,6 +15,23 @@ public class calculateDayOfWeekTest {
 	@Test
 	public void testOfWeek1() throws Exception {
 		Model model = new Model();
+		String stringDate1 = "2017-12-04";
+		String stringDate2 = "2018-01-03";
+		LocalDate startDate = model.stringToDate(stringDate1);
+		LocalDate endDate = model.stringToDate(stringDate2);
+		String stringTime1 = "09:00:00";
+		String stringTime2 = "14:00:00";
+		LocalTime startTime = model.stringToTime(stringTime1);
+		LocalTime endTime = model.stringToTime(stringTime2);
+		int duration = 30;
+		int secretCode = 1111;
+		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		assertEquals(schedule.calculateDayOfWeek(startDate),1);
+	}
+	
+	@Test
+	public void testOfWeek2() throws Exception {
+		Model model = new Model();
 		String stringDate1 = "2017-12-05";
 		String stringDate2 = "2018-01-03";
 		LocalDate startDate = model.stringToDate(stringDate1);
@@ -30,9 +47,9 @@ public class calculateDayOfWeekTest {
 	}
 	
 	@Test
-	public void testOfWeek2() throws Exception {
+	public void testOfWeek3() throws Exception {
 		Model model = new Model();
-		String stringDate1 = "2017-12-05";
+		String stringDate1 = "2017-12-06";
 		String stringDate2 = "2018-12-02";
 		LocalDate startDate = model.stringToDate(stringDate1);
 		LocalDate endDate = model.stringToDate(stringDate2);
@@ -43,7 +60,75 @@ public class calculateDayOfWeekTest {
 		int duration = 30;
 		int secretCode = 1111;
 		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
-		assertEquals(schedule.calculateDayOfWeek(endDate),7);
+		assertEquals(schedule.calculateDayOfWeek(startDate),3);
+	}
+	
+	@Test
+	public void testOfWeek4() throws Exception {
+		Model model = new Model();
+		String stringDate1 = "2017-12-07";
+		String stringDate2 = "2018-01-03";
+		LocalDate startDate = model.stringToDate(stringDate1);
+		LocalDate endDate = model.stringToDate(stringDate2);
+		String stringTime1 = "09:00:00";
+		String stringTime2 = "14:00:00";
+		LocalTime startTime = model.stringToTime(stringTime1);
+		LocalTime endTime = model.stringToTime(stringTime2);
+		int duration = 30;
+		int secretCode = 1111;
+		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		assertEquals(schedule.calculateDayOfWeek(startDate),4);
+	}
+	
+	@Test
+	public void testOfWeek5() throws Exception {
+		Model model = new Model();
+		String stringDate1 = "2017-12-08";
+		String stringDate2 = "2018-01-03";
+		LocalDate startDate = model.stringToDate(stringDate1);
+		LocalDate endDate = model.stringToDate(stringDate2);
+		String stringTime1 = "09:00:00";
+		String stringTime2 = "14:00:00";
+		LocalTime startTime = model.stringToTime(stringTime1);
+		LocalTime endTime = model.stringToTime(stringTime2);
+		int duration = 30;
+		int secretCode = 1111;
+		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		assertEquals(schedule.calculateDayOfWeek(startDate),5);
+	}
+	
+	@Test
+	public void testOfWeek6() throws Exception {
+		Model model = new Model();
+		String stringDate1 = "2017-12-09";
+		String stringDate2 = "2018-01-03";
+		LocalDate startDate = model.stringToDate(stringDate1);
+		LocalDate endDate = model.stringToDate(stringDate2);
+		String stringTime1 = "09:00:00";
+		String stringTime2 = "14:00:00";
+		LocalTime startTime = model.stringToTime(stringTime1);
+		LocalTime endTime = model.stringToTime(stringTime2);
+		int duration = 30;
+		int secretCode = 1111;
+		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		assertEquals(schedule.calculateDayOfWeek(startDate),6);
+	}
+	
+	@Test
+	public void testOfWeek7() throws Exception {
+		Model model = new Model();
+		String stringDate1 = "2017-12-10";
+		String stringDate2 = "2018-01-03";
+		LocalDate startDate = model.stringToDate(stringDate1);
+		LocalDate endDate = model.stringToDate(stringDate2);
+		String stringTime1 = "09:00:00";
+		String stringTime2 = "14:00:00";
+		LocalTime startTime = model.stringToTime(stringTime1);
+		LocalTime endTime = model.stringToTime(stringTime2);
+		int duration = 30;
+		int secretCode = 1111;
+		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		assertEquals(schedule.calculateDayOfWeek(startDate),7);
 	}
 }
 
--- TeamWezenSchedule/src/test/java/com/schedule/wezen/model/divideByWeekTest.java
+++ TeamWezenSchedule/src/test/java/com/schedule/wezen/model/divideByWeekTest.java
@@ -26,9 +26,9 @@ public class divideByWeekTest {
 		int duration = 30;
 		int secretCode = 1111;
 		Schedule schedule = new Schedule(startDate, endDate, startTime, endTime, duration, "id", secretCode);
-		ArrayList<Schedule> weeklySchedules = schedule.divideByWeeks(startDate, endDate, startTime, endTime, duration, "id", secretCode);
+		ArrayList<Schedule> weeklySchedules = schedule.divideByWeeks(/*startDate, endDate, startTime, endTime, duration, "id", secretCode*/);
 		int counter = 0;
-		for(Schedule s:weeklySchedules) {
+		for(Schedule s : weeklySchedules) {
 			counter++;
 		}
 		assertEquals(counter,5);
