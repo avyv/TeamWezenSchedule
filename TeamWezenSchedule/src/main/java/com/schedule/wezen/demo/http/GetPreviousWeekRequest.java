4f41cb9fe1bc3f5404b63973fe10278520551421
avyannaconevargas <avyannaconevarga@wpi.edu>
2018-12-11 17:42:50 -0500

    5

--- TeamWezenSchedule/src/main/java/com/schedule/wezen/demo/http/GetPreviousWeekRequest.java
+++ TeamWezenSchedule/src/main/java/com/schedule/wezen/demo/http/GetPreviousWeekRequest.java
@@ -4,9 +4,21 @@ public class GetPreviousWeekRequest {
 	public final String requestSchedID;
 	public final String requestWeekStart;
 	
-	public GetPreviousWeekRequest (String id, String ws) {
+	public final String requestWeekDay;
+	public final String requestMonth;
+	public final String requestYear;
+	public final String requestDate;
+	public final String requestTime;
+	
+	public GetPreviousWeekRequest (String id, String ws, String wd, String m, String y, String d, String t) {
 		this.requestSchedID = id;
 		this.requestWeekStart = ws;
+		
+		this.requestWeekDay = wd;
+		this.requestMonth = m;
+		this.requestYear = y;
+		this.requestDate = d;
+		this.requestTime = t;
 	}
 	
 	public String toString () {
